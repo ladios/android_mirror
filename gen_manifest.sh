57b4f266ddee68d663ed7694657bfeb9f1e24a79
Ladios Jonquil <ladios.jonquil@gmail.com>
2011-11-16 22:58:04 +0800

    Add gen_manifest.sh

---
+++ gen_manifest.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+_get_projs() {
+	curl "$1" | grep -E "/$2"'/([^"]+)">\1' \
+	| sed -e 's#[^>]*>\([^<]*\)<.*#'"$2/"'\1#' -e 's#^'"$2/$2"'$##' \
+	| grep -E '^.+'
+}
+
+get_projs() {
+	i=0
+	while [ 1 ]; do
+		i=$(($i + 1))
+		URL="https://github.com/$1/repositories/?page=$i"
+		echo >&2 "$URL"
+		PROJS=`_get_projs "$URL" $1`
+		echo >&2 ""
+		if [ $(echo "$PROJS" | wc -l) -eq 1 ]; then
+			break
+		fi
+		echo "$PROJS"
+	done
+}
+
+HEAD=$((`grep -n '<project ' manifest.xml | head -n 1 | cut -d ':' -f 1` - 1))
+TAIL=$((`grep -n '<project ' manifest.xml | tail -n 1 | cut -d ':' -f 1` + 1))
+
+head -n $HEAD manifest.xml 
+get_projs CyanogenMod | sed -e 's#\(.*\)#  <project name="\1" />#'
+tail -n +$TAIL manifest.xml
