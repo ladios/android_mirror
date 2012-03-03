c4bf5b69c0ca7a9d15a84d5ac67c5b9ecae1d549
Ladios Jonquil <ladios.jonquil@gmail.com>
2012-03-04 03:07:43 +0800

    Add proprietary_vendor_* projects

--- gen_manifest.sh
+++ gen_manifest.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 _get_projs() {
-	curl "$1" | grep -E "/$2"'/([^"]+)">\1' \
-	| sed -e 's#[^>]*>\([^<]*\)<.*#'"$2/"'\1#' -e 's#^'"$2/$2"'$##' \
-	| grep -E '^.+'
+	curl "$1" | grep -E "/$2"'/([^"]+)">\1' |
+	sed -e 's#[^>]*>\([^<]*\)<.*#'"$2/"'\1#' -e 's#^'"$2/$2"'$##' |
+	grep -E '^.+'
 }
 
 get_projs() {
@@ -21,9 +21,20 @@ get_projs() {
 	done
 }
 
+grep_projs() {
+	pattern="$1"
+	shift
+	for proj in $@; do
+		get_projs "$proj" |
+		grep "$pattern" |
+		sed -e 's#\(.*\)#  <project name="\1" />#'
+	done
+}
+
 HEAD=$((`grep -n '<project ' default.xml | head -n 1 | cut -d ':' -f 1` - 1))
 TAIL=$((`grep -n '<project ' default.xml | tail -n 1 | cut -d ':' -f 1` + 1))
 
 head -n $HEAD default.xml
-get_projs CyanogenMod | sed -e 's#\(.*\)#  <project name="\1" />#'
+grep_projs '.*' CyanogenMod
+grep_projs 'proprietary_vendor_' koush
 tail -n +$TAIL default.xml
